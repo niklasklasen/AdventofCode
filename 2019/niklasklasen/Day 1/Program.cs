ffdf151f56bca67912b1baff70711f86caa12921
Niklas Klasén <niklasklasen@evry.com>
2020-10-19 13:23:00 +0200

    created startingpoint for Console Application for Day 1 2019

---
+++ 2019/niklasklasen/Day 1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the start of Advent of Code 2019 Day 1 challange");
+        }
+    }
+}
