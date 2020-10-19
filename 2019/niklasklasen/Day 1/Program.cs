484db4c60b639b08b1a0bafdb3606256eddb0e9b
Niklas Klasén <niklasklasen@evry.com>
2020-10-20 00:06:12 +0200

    added selfcheck params

--- 2019/niklasklasen/Day 1/Program.cs
+++ 2019/niklasklasen/Day 1/Program.cs
@@ -7,6 +7,8 @@ namespace Day1
     {
         static void Main(string[] args)
         {
+            List<int> input = new List<int>() {12, 14, 1969, 100756};
+            
             // Calculation
             var mass = 100756;
             int fuel = (mass / 3) - 2;
@@ -14,6 +16,9 @@ namespace Day1
             // Output
             Console.WriteLine($"module mass: {mass}");
             Console.WriteLine($"Fuel requierd: {fuel}");
+
+            int ans = 2 + 2 + 654 + 33583;
+            Console.WriteLine($"Fuel requierd should be {ans}");
         }
     }
 }
