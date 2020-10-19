f4d01c0ead3d56464e1d7d0c8e962cd96d8d7899
Niklas Klasén <niklasklasen@evry.com>
2020-10-19 23:54:18 +0200

    cleanup comments

--- 2019/niklasklasen/Day 1/Program.cs
+++ 2019/niklasklasen/Day 1/Program.cs
@@ -6,12 +6,8 @@ namespace Day1
     {
         static void Main(string[] args)
         {
-            // Task
-            //Fuel required to launch a given module is based on its mass. 
-            //Specifically, to find the fuel required for a module, take its mass, divide by three, round down, and subtract 2.
-
             // Calculation
-            var mass = 12;
+            var mass = 100756;
             int fuel = (mass / 3) - 2;
 
             // Output
