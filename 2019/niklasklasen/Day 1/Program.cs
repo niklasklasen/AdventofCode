17b52ac8268fcaf0fdc5aa824a4daea4e81e5288
Niklas Klasén <niklasklasen@evry.com>
2020-10-21 00:38:17 +0200

    fixed av verified calculations.

--- 2019/niklasklasen/Day 1/Program.cs
+++ 2019/niklasklasen/Day 1/Program.cs
@@ -8,26 +8,17 @@ namespace Day1
         static void Main(string[] args)
         {
             // Mass input
-            List<int> massInput = new List<int>();
-            massInput.Add(12);
-            massInput.Add(14);
-            massInput.Add(1969);
-            massInput.Add(100756);
+            var massInput = new List<int>() {12, 14, 1969, 100756};
             
-            var fuelTotal = new List<decimal>();
-
             // Calculation
-            for(int i = 0; i < massInput.Count; i++)
+            var fuel = 0;
+            foreach (var module in massInput)
             {
-                int fuel = (massInput[i] / 3) - 2;
-                fuelTotal.Add(fuel);
-            };
-            int sum = fuelTotal.Sum();
-
+                fuel += (module / 3) -2;
+            }
+        
             // Output
-            Console.WriteLine(sum);
-            int ans = 2 + 2 + 654 + 33583;
-            Console.WriteLine($"Fuel requierd should be {ans}");
+            Console.WriteLine($"Fuel requierd is {fuel}");
         }
     }
 }
