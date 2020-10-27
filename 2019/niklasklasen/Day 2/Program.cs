c5b2a20f8f8649d569833498524a80e516cc3ae5
Niklas Klasén <niklasklasen@evry.com>
2020-10-27 23:53:27 +0100

    added optcode method

--- 2019/niklasklasen/Day 2/Program.cs
+++ 2019/niklasklasen/Day 2/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Day_2
 {
     class Program
     {
-        static void Main(string[] args)
+       public static int[] Optcode()
         {
-            Console.WriteLine("Hello World!");
+            int[] array = new int[] {1,0,0,0,99};
+            // array[1] = 12;
+            // array[2] = 2;
+            return array;
+        }
+
+        public static void Main(string[] args)
+        {
+            var numbs = Optcode();
+            foreach (var item in numbs)
+            {
+                System.Console.WriteLine(item);
+            }
         }
     }
 }
