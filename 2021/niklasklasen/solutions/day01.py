e1c175874091a1c0e57dfc4f894b3fc297efe90d
Niklas Klasén <klasen.niklas@gmail.com>
2021-12-10 00:12:17 +0100

    changed calculation, still same issue

--- 2021/niklasklasen/solutions/day01.py
+++ 2021/niklasklasen/solutions/day01.py
@@ -3,14 +3,14 @@ import advent
 readout = advent.input('2021_01')
 
 n = 0
-depthcount = 0
+depthcount = []
 nmax = len(readout) - 1
 
 while(n <= nmax):
     if readout[n] > readout[n-1]:
-        depthcount = depthcount + 1
+        depthcount.append(int(readout[n]))
     n = n + 1
-print(depthcount)
+print(len(depthcount))
 
 # Wrong Answers
 # 1296
